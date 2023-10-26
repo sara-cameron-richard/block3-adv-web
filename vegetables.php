2194f4636ca2526ef4eb90aae2cc7fc343b0e36c
sara-cameron-richard <134742947+sara-cameron-richard@users.noreply.github.com>
2023-10-26 13:43:29 -0400

    changes 1:43 oct 26

--- vegetables.php
+++ vegetables.php
@@ -2,6 +2,7 @@
 
 ini_set('display_errors', 1);
 
+//1.create a class Vegetable with public properties
 class Vegetable
 {
     public $name;
@@ -11,8 +12,42 @@ class Vegetable
     public $nutrition;
     public $propagation;
     public $method;
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getAmount()
+    {
+        return $this->amount;
+    }
+
+    public function getAge()
+    {
+        return $this->age;
+    }
+
+    public function getMethod()
+    {
+        return $this->method;
+    }
+
+    public function determineAction()
+    {
+        if ($this->age == 1) {
+            return "store this for later";
+        } elseif ($this->age == 2) {
+            return "cook this now";
+        } elseif ($this->age == 3) {
+            return "compost this, quickly";
+        } else {
+            return "Not sure what to do with this vegetable";
+        }
+    }
 }
 
+//2. instiantiate the class by creating 3 new objects
 $carrot = new Vegetable();
 $carrot->name = 'carrot';
 $carrot->amount = '100g';
@@ -21,8 +56,6 @@ $carrot->age = '1';
 $carrot->nutrition = 'carotene';
 $carrot->propagation = 'seed';
 $carrot->method = 'roast';
-
-echo $carrot->name, $carrot->amount;
 
 $spinach = new Vegetable();
 $spinach->name = 'spinach';
@@ -33,8 +66,6 @@ $spinach->nutrition = 'iron';
 $spinach->propagation = 'seed';
 $spinach->method = 'steam';
 
-echo $spinach->name, $spinach->taste;
-
 $zucchini = new Vegetable();
 $zucchini->name = 'zucchini';
 $zucchini->amount = '300g';
@@ -44,4 +75,11 @@ $zucchini->nutrition = 'potassium';
 $zucchini->propagation = 'seed';
 $zucchini->method = 'bake';
 
-echo $zucchini->name, $zucchini->age;
+//3. test that the properties are set correctly
+// echo $carrot->name, " weighs =", $carrot->amount;
+// echo $spinach->name, " tastes ", $spinach->taste;
+// echo $zucchini->name, " age is ", $zucchini->age;
+
+echo "this vegetable is ", $carrot->name, " and you should ", $carrot->determineAction(), " . "; 
+echo "this vegetable is ", $spinach->name, " and you should ", $spinach->determineAction(), " . ";
+echo "this vegetable is ", $zucchini->name, " and you should ", $zucchini->determineAction();
